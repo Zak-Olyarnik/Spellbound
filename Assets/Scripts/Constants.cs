9e06613c3b0e9af15bf61741d89324e75ec21c7b
Zak-Olyarnik <zwo24@drexel.edu>
2018-08-22 16:28:41 -0400

    replacing duplicate "g" with "j" in letter list

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -12,10 +12,10 @@ public class Constants : MonoBehaviour
     public static Sprite[] cloudSprites;
     public static string[] letters = new string[] {
         "A", "E", "I", "O", "U",         //vowels, ind 0-4 
-        "B", "C", "D", "F", "G", "H", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z",    // consonants, ind 5-24
-        "BL", "BR", "CH", "CK", "CL", "CR", "DR", "FL", "FR", "GH", "GL", "GR", "NG", "PH", "PL", "PR", "QU", "SC", "SH", "SK", "SL", "SM", "SN", "SP", "ST", "SW", "TH", "TR", "TW", "WH", "WR",   // digraphs, ind 25-55
-        "NTH", "SCH", "SCR", "SHR", "SPL", "SPR", "SQU", "STR", "THR",  // trigraphs, ind 56-64
-        "AI", "AU", "AW", "AY", "EA", "EE", "EI", "EU", "EW", "EY", "IE", "OI", "OO","OU", "OW", "OY" }; // vowel digraphs, ind 65-80
+        "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z",    // consonants, ind 5-25
+        "BL", "BR", "CH", "CK", "CL", "CR", "DR", "FL", "FR", "GH", "GL", "GR", "NG", "PH", "PL", "PR", "QU", "SC", "SH", "SK", "SL", "SM", "SN", "SP", "ST", "SW", "TH", "TR", "TW", "WH", "WR",   // digraphs, ind 26-56
+        "NTH", "SCH", "SCR", "SHR", "SPL", "SPR", "SQU", "STR", "THR",  // trigraphs, ind 57-65
+        "AI", "AU", "AW", "AY", "EA", "EE", "EI", "EU", "EW", "EY", "IE", "OI", "OO","OU", "OW", "OY" }; // vowel digraphs, ind 66-81
     //TODO: "ER", "AR", "OR", etc., "ING", "ED"...
     public static Color[] potionColors = new Color[] { Color.blue, Color.green, Color.red, Color.yellow };
 
